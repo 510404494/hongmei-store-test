2abaafda3551b373635c32940c26536489626b56
konglingqiang <510404494@qq.com>
2019-11-21 17:31:58 +0800

    无报错

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import '@/styles/element-variables.scss'
 import '@/styles/index.scss'
 
 import App from './App.vue'
-import store from './store'
+// import store from './store'
 import router from './router'
 import { AppModule } from '@/store/modules/app'
 import i18n from '@/lang'
@@ -41,8 +41,7 @@ Object.keys(filters).forEach(key => {
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
+  router, // store,
   i18n,
   render: h => h(App)
 }).$mount('#app')
